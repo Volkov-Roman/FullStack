59aa270e353b373044a0f0a520d9134b6c34b179
Roman Volkov <rvolkovpost@gmail.com>
2025-01-06 21:00:46 +0200

    2.2

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -3,6 +3,7 @@ const Course = ({ course }) => {
     <div>
       <Header course={course.name} />
       <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 }
@@ -17,6 +18,11 @@ const Content = ({ parts }) => {
       {parts.map(part => (<Part key={part.id} part={part.name} number_of_exercises={part.exercises} />))}
     </div>
   )
+}
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0);
+  return <p style={{ fontWeight: 'bold' }}>total of {total} exercises</p>;
 }
 
 const Part = ({ part, number_of_exercises }) => {
