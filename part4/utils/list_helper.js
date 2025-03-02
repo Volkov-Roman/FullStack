6499b31a04657a50a2de703c181129f30bd40201
Roman Volkov <rvolkovpost@gmail.com>
2025-03-02 22:12:46 +0200

    4.5

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -4,4 +4,16 @@ const totalLikes = (blogs) => {
     return blogs.reduce((sum, blog) => sum + blog.likes, 0)
   }
 
-module.exports = { dummy, totalLikes }
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null
+
+  const topBlog = blogs.reduce((max, blog) => (blog.likes > max.likes ? blog : max), blogs[0])
+
+  return {
+    title: topBlog.title,
+    author: topBlog.author,
+    likes: topBlog.likes
+  }
+}
+
+module.exports = { dummy, totalLikes, favoriteBlog }
