bbb74b22981295053ecb74227a4822d799f59924
Roman Volkov <rvolkovpost@gmail.com>
2025-03-02 22:21:47 +0200

    4.6

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => 1
 
 const totalLikes = (blogs) => {
@@ -16,4 +18,16 @@ const favoriteBlog = (blogs) => {
   }
 }
 
-module.exports = { dummy, totalLikes, favoriteBlog }
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) return null
+  
+  const authorCounts = _.countBy(blogs, 'author')
+  const topAuthor = _.maxBy(Object.keys(authorCounts), (author) => authorCounts[author])
+  
+  return {
+    author: topAuthor,
+    blogs: authorCounts[topAuthor]
+  }
+}
+
+module.exports = { dummy, totalLikes, favoriteBlog, mostBlogs }
