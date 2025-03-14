7f3ae92ecbefd7c8b0dfbe13d2805d75be4b4b97
Roman Volkov <rvolkovpost@gmail.com>
2025-03-14 10:25:18 +0200

    4.15

---
+++ part4/tests/users_api.test.js
@@ -0,0 +1,68 @@
+const { test, beforeEach, describe, after } = require('node:test')
+const mongoose = require('mongoose')
+const assert = require('node:assert')
+const supertest = require('supertest')
+const app = require('../app')
+const helper = require('./test_helper')
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
+
+const api = supertest(app)
+
+describe('when there is initially one user in db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('medved', 10)
+    const user = new User({ username: 'root', passwordHash })
+
+    await user.save()
+  })
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'susinen',
+      name: 'Roman Volkov',
+      password: 'password',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map(u => u.username)
+    assert(usernames.includes(newUser.username))
+  })
+
+  test('creation fails with proper statuscode and message if username already taken', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'root',
+      name: 'Supersusi',
+      password: 'volchara',
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    assert(result.body.error.includes('expected `username` to be unique'))
+
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length)
+  })
+})
+
+after(async () => {
+    await mongoose.connection.close()
+})
--- part4/utils/middleware.js
+++ part4/utils/middleware.js
@@ -12,11 +12,13 @@ const unknownEndpoint = (req, res) => {
     res.status(404).send({ error: 'Unknown endpoint' })
   }
   
-  const errorHandler = (error, req, res, next) => {
+  const errorHandler = (error, request, response, next) => {
     logger.error(error.message)
 
     if (error.name === 'ValidationError') {
       return response.status(400).json({ error: error.message })
+    } else if (error.name === 'MongoServerError' && error.message.includes('E11000 duplicate key error')) {
+      return response.status(400).json({ error: 'expected `username` to be unique' })
     }
 
     next(error)
