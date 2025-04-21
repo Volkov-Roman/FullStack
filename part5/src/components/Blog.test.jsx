b8e8373b51800d71b9c3363bd2e28ed48599832b
Roman Volkov <rvolkovpost@gmail.com>
2025-04-21 14:31:34 +0300

    5.14

--- part5/src/components/Blog.test.jsx
+++ part5/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders blog title and author, but not url or likes by default', () => {
@@ -20,3 +21,21 @@ test('renders blog title and author, but not url or likes by default', () => {
   expect(urlElement).toBeNull()
   expect(likesElement).toBeNull()
 })
+
+test('shows blog URL and likes when details button is clicked', async () => {
+    const blog = {
+      title: 'Testing React components',
+      author: 'Roma',
+      url: 'http://example.com',
+      likes: 420
+    }
+  
+    render(<Blog blog={blog} />)
+  
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+  
+    expect(screen.getByText('http://example.com')).toBeDefined()
+    expect(screen.getByText(/420/)).toBeDefined()
+  })
